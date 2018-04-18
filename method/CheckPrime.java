f7c285ce2cce3c485186514d3e21c70ba5abb0d0
Administrator <dol.mouse92@gmail.com>
2018-04-18 17:01:28 +0700

    Kiểm tra số nguyên tố

---
+++ method/CheckPrime.java
@@ -0,0 +1,25 @@
+package method;
+
+import java.util.Scanner;
+
+public class CheckPrime {
+    public static void main(String[] args) {
+        int number;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        number = sc.nextInt();
+        System.out.println(checkPrimes(number));
+    }
+
+    public static boolean checkPrimes(int n) {
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i < Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

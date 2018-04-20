58ad6a2d5d3f9b61d33f644147094a27e63280aa
Administrator <dol.mouse92@gmail.com>
2018-04-20 08:48:55 +0700

    Xây dựng lớp My Integer

---
+++ MyInteger/App.java
@@ -0,0 +1,82 @@
+package MyInteger;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter value: ");
+        int value = input.nextInt();
+
+        displayNonParameter(value);
+
+        displayHasParameter(value);
+
+        MyInteger number = new MyInteger(value);
+        displayParameterIsObject(number);
+
+        displayCompareParamaterInt(value);
+
+        displayCompareParameterObject(value);
+
+        displayParseInt();
+
+        displayParseIntString();
+    }
+
+    public static void displayNonParameter(int value){
+        MyInteger myInterger1 = new MyInteger(value);
+
+        System.out.println("display method non parameter");
+        System.out.println("number is event " + myInterger1.isEven());
+        System.out.println("number is odd " + myInterger1.isOdd());
+        System.out.println("number is prime " + myInterger1.isPrime());
+    }
+
+    public static void displayHasParameter(int value){
+        System.out.println("display method has parameter");
+        System.out.println("number is event " + MyInteger.isEven(value));
+        System.out.println("number is odd " + MyInteger.isOdd(value));
+        System.out.println("number is prime " + MyInteger.isPrime(value));
+    }
+
+    public static void displayParameterIsObject(MyInteger number){
+        System.out.println("display method has parameter is object");
+        System.out.println("number is event " + MyInteger.isEven(number));
+        System.out.println("number is odd " + MyInteger.isOdd(number));
+        System.out.println("number is prime " + MyInteger.isPrime(number));
+    }
+
+    public static void displayCompareParamaterInt(int value){
+        MyInteger compareNumber = new MyInteger(5);
+
+        System.out.println("display method compare has parameter int");
+        System.out.println("is value equals number " + compareNumber.equals(value));
+    }
+
+    public static void displayCompareParameterObject(int value){
+        MyInteger numberObject = new MyInteger();
+        numberObject.setValue(8);
+        MyInteger compareNumber = new MyInteger(value);
+        System.out.println("display method compare has parameter object");
+        System.out.println("is value equals number " + compareNumber.equals(numberObject));
+    }
+
+    public static void displayParseInt(){
+        char[] listCharacter = {1, 'a', 7, 9};
+        System.out.println("display method parseInt has parameter char[] ");
+        System.out.println("List number ");
+        for (int i = 0; i < listCharacter.length; i++) {
+            System.out.print(MyInteger.parseInt(listCharacter)[i] + " ");
+        }
+    }
+
+    public static void displayParseIntString(){
+        String convertString = "name";
+        System.out.println("\ndisplay method parseInt has parameter string");
+        for (int i = 0; i < convertString.length(); i++) {
+            System.out.print(MyInteger.parseInt(convertString)[i] + " ");
+        }
+    }
+}
---
+++ MyInteger/MyInteger.java
@@ -0,0 +1,154 @@
+package MyInteger;
+
+import java.util.Objects;
+
+public class MyInteger {
+    private int value;
+
+    public MyInteger() {
+    }
+
+    public MyInteger(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public boolean isEven() {
+        if (this.value % 2 == 0) {
+            return true;
+        } else
+            return false;
+    }
+
+    public boolean isOdd() {
+        if (this.value % 2 != 0)
+            return true;
+        else
+            return false;
+    }
+
+    public boolean isPrime() {
+        if (this.value < 2) {
+            return false;
+        } else {
+            int i = 2;
+            boolean check = true;
+            while (i <= Math.sqrt(this.value)) {
+                if (this.value % i == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    public static boolean isEven(int value) {
+        if (value % 2 == 0) {
+            return true;
+        } else
+            return false;
+    }
+
+    public static boolean isOdd(int value) {
+        if (value % 2 != 0)
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean isPrime(int value) {
+        if (value < 2) {
+            return false;
+        } else {
+            int i = 2;
+            boolean check = true;
+            while (i <= Math.sqrt(value)) {
+                if (value % i == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    public static boolean isEven(MyInteger value) {
+        if (value.getValue() % 2 == 0) {
+            return true;
+        } else
+            return false;
+    }
+
+    public static boolean isOdd(MyInteger value) {
+        if (value.getValue() % 2 != 0)
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean isPrime(MyInteger value) {
+        if (value.getValue() < 2) {
+            return false;
+        } else {
+            int i = 2;
+            boolean check = true;
+            while (i <= Math.sqrt(value.getValue())) {
+                if (value.getValue() % i == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    public boolean equals(int value) {
+        if (this.getValue() == value) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean equals(MyInteger value) {
+        if (this.getValue() == value.getValue()) {
+            return true;
+        }
+        return false;
+    }
+
+    public static int[] parseInt(char[] listCharacters) {
+        int[] listNumber = new int[listCharacters.length];
+        for (int i = 0; i < listCharacters.length; i++) {
+            listNumber[i] = (int) listCharacters[i];
+        }
+        return listNumber;
+    }
+
+    public static int[] parseInt(String convertString) {
+        int[] number = new int[convertString.length()];
+        for (int i = 0; i < convertString.length(); i++) {
+            number[i] = (int) convertString.charAt(i);
+        }
+        return number;
+    }
+}
